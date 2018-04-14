c728508750b18ca02ce959b333e599dc01a1bed7
Klomi <2947133416@qq.com>
2018-04-14 11:12:20 +0800

    2018/04/14
    将其他分支上的改动移植到ailocal分支
    1. 改变远程上传项目功能按钮在菜单里的位置
    2. LocalAccessFilter的改动
    3. OdeAuthFilter找不到用户时的操作

--- appinventor/appengine/src/com/google/appinventor/server/LocalAccessFilter.java
+++ appinventor/appengine/src/com/google/appinventor/server/LocalAccessFilter.java
@@ -12,26 +12,36 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.*;
 import java.net.*;
+import java.util.logging.Logger;
 
 public class LocalAccessFilter implements Filter{
+    private static final Logger LOG = Logger.getLogger(OdeAuthFilter.class.getName());
     private Set<String> localAddresses = new HashSet<>();
     
     @Override
     public void init(FilterConfig config){
         try{
-            localAddresses.add(InetAddress.getLocalHost().getHostAddress());
-            for(InetAddress addr : InetAddress.getAllByName("localhost"))
-                localAddresses.add(addr.getHostAddress());
+            Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
+            while(e.hasMoreElements()){
+                NetworkInterface networkInterfaces = e.nextElement();
+                Enumeration<InetAddress> addresses = networkInterfaces.getInetAddresses();
+                while(addresses.hasMoreElements())
+                    localAddresses.add(addresses.nextElement().getHostAddress());
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
+
+        LOG.info("LocalAddresses:");
+        for(String addr : localAddresses)
+            LOG.info(addr);
     }
     
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpReq = (HttpServletRequest)req;
         HttpServletResponse httpResp = (HttpServletResponse)resp;
-        System.err.printf("[%s] Access from %s\n", httpReq.getRequestURI(), httpReq.getRemoteAddr());
+        LOG.info("[" + httpReq.getRequestURI() + "] Access from " + httpReq.getRemoteAddr());
         
         if(localAddresses.contains(httpReq.getRemoteAddr()))
             chain.doFilter(req, resp);
